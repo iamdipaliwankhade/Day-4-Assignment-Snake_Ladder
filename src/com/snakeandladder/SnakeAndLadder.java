5cbe19de92fb20e3c71c70d7f9cabcf5f0117e41
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-23 17:41:09 +0530

    [Dipali] Added : Number between 1 to 6

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -10,7 +10,8 @@ public class SnakeAndLadder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake Ladder Program");
 		int playerPosition = 0;
-		System.out.println("Position of the player Before Game start position : " + playerPosition);
+		System.out.println("Position of the player Before Game start : " + playerPosition);
+		int dice = (int) (Math.floor((Math.random() * 6) + 1));
+		System.out.println("Rolled Dice Number is " + dice);
 	}
-
 }
