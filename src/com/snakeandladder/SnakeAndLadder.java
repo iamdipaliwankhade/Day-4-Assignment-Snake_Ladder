254665efc247142d7c727c71b9cc155bb5ec64fd
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-23 17:59:58 +0530

    [Dipali] Added : While two players

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -8,49 +8,106 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 	public static final int WINNING_POSITION = 100;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Snake Ladder Program");
-		System.out.println("*******************************\n");
-		int playerPosition = 0;
-		int i = 0;
-		System.out.println("Position of the player Before Game start : " + playerPosition);
-		while (playerPosition != WINNING_POSITION) {
-			System.out.println("\nRoll Dice to Move");
-			System.out.println("*****************");
-			int dice = (int) (Math.floor((Math.random() * 6) + 1));
-			System.out.println("The Dice is : " + dice);
-			int chance = (int) Math.floor(Math.random() * 3);
-			if (chance == 0) {
-				System.out.println("No Play");
-				playerPosition = playerPosition;
-				i++;
-			} else if (chance == 1) {
-				System.out.println("Got a Ladder");
-				if (playerPosition < WINNING_POSITION) {
-					playerPosition = playerPosition + dice;
-					i++;
-					if (playerPosition > WINNING_POSITION) {
-						System.out.println("position getting more than 100");
-						playerPosition = playerPosition - dice;
-					}
-				} else {
-					playerPosition = playerPosition;
-					i++;
-				}
-			} else {
-				System.out.println("Hit with a Snake");
-				playerPosition = playerPosition - dice;
-				i++;
-				if (playerPosition < 0) {
-					playerPosition = 0;
+	static int playerOnePosition = 0;
+	static int playerTwoPosition = 0;
+	static int dice1, dice2;
+	static int count_player_1 = 0;
+	static int count_player_2 = 0;
 
-				}
-			}
-			System.out.println("Move : " + i);
-			System.out.println("player present Position is " + playerPosition);
-		}
-		System.out.println("");
-		System.out.println("Moves required to finish Game is :" + i);
+	// Main function
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Snake-ladder program");
+		System.out.println("*******************************");
+		DiceRoll();
 	}
+
+	// OptionCheck for Player 1
+	public static void OptionCheck1() {
+		int optionCheck = (int) (Math.floor(Math.random() * 3));
+		switch (optionCheck) {
+		case 0:
+			System.out.println("So its No Play");
+			System.out.println("Players position would be :" + playerOnePosition);
+			break;
+		case 1:
+			System.out.println("its a Ladder");
+			playerOnePosition = playerOnePosition + dice1;
+			if (playerOnePosition > WINNING_POSITION) {
+				playerOnePosition = playerOnePosition - dice1;
+			}
+			break;
+
+		case 2:
+			System.out.println("its a Snake");
+			playerOnePosition = playerOnePosition - dice1;
+			if (playerOnePosition < 0) {
+				playerOnePosition = 0;
+				break;
+			}
+		}
+	}
+
+	// Options check for Player 2
+	public static void OptionCheck2() {
+		int optionCheckTwo = (int) (Math.floor(Math.random() * 3));
+		switch (optionCheckTwo) {
+		case 0:
+			System.out.println("So its No Play");
+			System.out.println("Players position would be :" + playerTwoPosition);
+			break;
+
+		case 1:
+			System.out.println("its a Ladder");
+			playerTwoPosition = playerTwoPosition + dice2;
+			if (playerTwoPosition > WINNING_POSITION) {
+				playerTwoPosition = playerTwoPosition - dice2;
+			}
+			break;
+
+		case 2:
+			System.out.println("its a Snake");
+			playerTwoPosition = playerTwoPosition - dice2;
+			if (playerTwoPosition < 0) {
+				playerTwoPosition = 0;
+				break;
+			}
+		}
+
+	}
+
+	// Dice Roll for Both Players
+	public static void DiceRoll() {
+
+		System.out.println("Initial position of the player is :" + playerOnePosition + "and" + playerTwoPosition);
+
+		while (playerOnePosition != WINNING_POSITION && playerTwoPosition != WINNING_POSITION) {
+
+			// Player 1
+			count_player_1++;
+			System.out.println("Moves of Player 1 : " + count_player_1);
+			dice1 = (int) (Math.floor((Math.random() * 6) + 1));
+			OptionCheck1();
+			System.out.println("player 1 position is " + playerOnePosition);
+			System.out.println("-------------------------------");
+
+			// Player 2
+			count_player_2++;
+			System.out.println("Moves of Player 2 : " + count_player_2);
+			dice2 = (int) (Math.floor((Math.random() * 6) + 1));
+			OptionCheck2();
+			System.out.println("player 2 position is" + playerTwoPosition);
+			System.out.println("-------------------------------");
+
+		}
+
+		if (playerOnePosition == WINNING_POSITION) {
+			System.out.println("Player 1 won the game and Took" + " " + count_player_1 + " Moves");
+		}
+
+		else {
+			System.out.println("Player 2 won the game and Took" + " " + count_player_2 + "Moves");
+		}
+	}
+
 }
