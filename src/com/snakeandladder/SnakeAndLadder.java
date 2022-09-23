c89d15082b42acb51533fd2a0b7b18cfba559b37
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-23 17:48:57 +0530

    [Dipali] Added : winning position 100

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -6,32 +6,36 @@ package com.snakeandladder;
  *
  */
 public class SnakeAndLadder {
+	public static final int WINNING_POSITION = 100;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake Ladder Program");
 		System.out.println("*******************************\n");
 		int playerPosition = 0;
 		System.out.println("Position of the player Before Game start : " + playerPosition);
-		int dice = (int) (Math.floor((Math.random() * 6) + 1));
-		System.out.println("Rolled Dice Number is " + dice);
-
-		int chance = (int) Math.floor(Math.random() * 3);
-		if (chance == 0) {
-			System.out.println("No Play");
-			playerPosition = playerPosition;
-			System.out.println("player Position after roll is " + playerPosition);
-		} else if (chance == 1) {
-			System.out.println("Got a Ladder");
-			playerPosition = playerPosition + dice;
-			System.out.println("player Position after roll is " + playerPosition);
-		} else {
-			System.out.println("Hit with a Snake");
-			playerPosition = playerPosition - dice;
-			if (playerPosition < 0) {
-				playerPosition = 0;
+		while (playerPosition != WINNING_POSITION) {
+			System.out.println("Roll Dice to Move");
+			int dice = (int) (Math.floor((Math.random() * 6) + 1));
+			int chance = (int) Math.floor(Math.random() * 3);
+			if (chance == 0) {
+				System.out.println("No Play");
+				playerPosition = playerPosition;
+			} else if (chance == 1) {
+				System.out.println("Got a Ladder");
+				if (playerPosition < WINNING_POSITION) {
+					playerPosition = playerPosition + dice;
+				} else {
+					playerPosition = playerPosition;
+				}
+			} else {
+				System.out.println("Hit with a Snake");
+				playerPosition = playerPosition - dice;
+				if (playerPosition < 0) {
+					playerPosition = 0;
+				}
 			}
-			System.out.println("player Position after roll is" + playerPosition);
+			System.out.println("player present Position is " + playerPosition);
 		}
 	}
-
 }
