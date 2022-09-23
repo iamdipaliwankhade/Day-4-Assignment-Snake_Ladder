92f271c7642323513bc7f351f97e5e40b0384e15
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-23 17:37:41 +0530

    [Dipali] Added : Game player with single player

---
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,16 @@
+package com.snakeandladder;
+
+/**
+ * 
+ * @author Dipali
+ *
+ */
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake Ladder Program");
+		int playerPosition = 0;
+		System.out.println("Position of the player Before Game start position : " + playerPosition);
+	}
+
+}
